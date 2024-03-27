9109154caf0e02767da1aa5b27243cfb6902bb44
draganjareb <dragan931@live.com>
2024-03-27 18:56:45 +0100

    change in home špage

--- src/features/Work/Work.jsx
+++ src/features/Work/Work.jsx
@@ -2,10 +2,7 @@ const Work = () => {
   return (
     <div>
       <h1>Page Work</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
-      </p>
+      <p>Lorem text of the printing and typesetting industry.</p>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry.
